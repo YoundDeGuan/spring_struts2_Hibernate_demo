76d76fc0b6ca1d9134d27b5e0f8a8fb0a5f28dd8
deyound <13924479530@163.com>
2017-05-23 09:27:16 +0800

    Initial commit

---
+++ src/test/java/org/andy/shop/service/TestUserService.java
@@ -0,0 +1,42 @@
+package org.andy.shop.service;
+
+import com.alibaba.fastjson.JSON;
+import org.andy.shop.entity.UserInfo;
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * 创建时间：2015-2-13 下午3:31:07
+ * 
+ * @author andy
+ * @version 2.2
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:spring.xml",
+		"classpath:spring-hibernate.xml" })
+public class TestUserService {
+
+	private static final Logger LOGGER = Logger
+			.getLogger(TestUserService.class);
+
+	@Autowired
+	private UserInfoService userInfoService;
+
+	@Test
+	public void save() {
+		UserInfo userInfo = new UserInfo();
+		userInfo.setName("k66k");
+		userInfo.setAge(23);
+		userInfo.setTelephone("13212221333");
+		Integer id = userInfoService.save(userInfo);
+		JSON.toJSONString(id);
+	}
+@Test
+	public void test(){
+		System.out.println("fdsafdsf");
+}
+}
